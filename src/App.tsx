1e1c9e36a2ba692fec8fa0f6b22e11182ebdbd8e
Prasshant Shanmugalingam <stickoking@hotmail.com>
2022-09-25 06:37:45 +0800

    Add todos Model, Handle adding todos function

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import React, { useState } from 'react'
 import './App.css'
 import InputField from './components/InputField'
+import { Todo } from './models'
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>('')
-  console.log(todo)
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  const handleAdd = (e: React.FormEvent): void => {
+    e.preventDefault()
+    if (todo !== '') {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }])
+      setTodo('')
+    }
+  }
+  console.log(todos)
   return (
     <div className='app'>
       <span className='heading'>Todo</span>
-      <InputField todo={ todo } setTodo={ setTodo } />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
     </div>
   )
 }
